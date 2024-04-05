ae42286d0a04e5c18affc7b3cf27c1de1b64e160
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2024-04-05 16:40:04 +0100

    tuning done

--- src/warp_utils.py
+++ src/warp_utils.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 import numpy as np
 import warp.sim.render as wpr
 
+epsilon = 1e-6
+
 def render_usd(stage, model, states, sim_duration, sim_dt):
     """
     Render a physics simulation to USD using Warp's rendering API.
@@ -26,9 +28,9 @@ def render_usd(stage, model, states, sim_duration, sim_dt):
         sim_dt = 0.1  # Time step of the simulation (in seconds)
         usd_render(model_path, states, sim_duration, sim_dt)
     """
-    renderer = wpr.SimRenderer(model, stage, scaling=1.0)
+    renderer = wpr.SimRenderer(model, stage, scaling=1.0, up_axis='Z')
     
-    sim_time = np.arange(0, sim_duration + sim_dt, sim_dt)
+    sim_time = np.arange(0, sim_duration + epsilon, sim_dt)
     for i in tqdm(range(len(sim_time))):
         renderer.begin_frame(sim_time[i])
         renderer.render(states[i])
